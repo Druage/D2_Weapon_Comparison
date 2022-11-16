021e44084b4d0b5af33afa505a52c90303991a37
Lee Lazarecky <leelazarecky@gmail.com>
2022-11-16 01:45:27 -0500

    Move compare and add buttons

--- src/components/ComparisonList.tsx
+++ src/components/ComparisonList.tsx
@@ -5,7 +5,11 @@ import {FAKE_WEAPONS_TO_TEST} from "../data/data";
 export default function ComparisonList() {
 
     return <div className={"absolute top-0 bottom-0 right-0 left-80 bg-yellow-500 p-8 overflow-scroll"}>
-        <h1 className={"text-center mb-8 text-2xl"}>RESULTS</h1>
+
+        <div className={"flex flex-row justify-between items-center mb-8"}>
+            <h1 className={"text-2xl"}>RESULTS</h1>
+            <button className={"h-full bg-white py-4 px-12 text-lg"}>Compare</button>
+        </div>
 
         <div className={"flex flex-col gap-4"}>
             {FAKE_WEAPONS_TO_TEST.map((it, index) => <WeaponItem key={index}
--- src/components/WeaponList.tsx
+++ src/components/WeaponList.tsx
@@ -4,18 +4,13 @@ import {FAKE_WEAPONS_TO_TEST} from "../data/data";
 
 export default function WeaponList() {
 
-    const AddWeaponButton = () => <Trigger
-        className={"bg-gray-800 h-20 text-white flex justify-center items-center mb-8 w-full"}>
-        ADD +
-    </Trigger>
-
-    const CompareButton = () => <div
-        className={"absolute bottom-0 left-0 right-0 bg-purple-400 h-20 flex justify-center items-center"}>COMPARE</div>
+    const CompareButton = () => <Trigger
+        className={"absolute bottom-0 left-0 right-0 bg-purple-400 h-20 flex justify-center items-center"}>Add
+        +</Trigger>
 
     return <div className={"absolute left-0 top-0 bottom-0 bg-red-500 w-80"}>
 
         <div className={"_scroll-area absolute overflow-scroll left-0 top-0 bottom-0 w-full p-4"}>
-            <AddWeaponButton/>
 
             <div className={"flex flex-col gap-4"}>
                 {FAKE_WEAPONS_TO_TEST.map((it, index) => <WeaponItem key={index}
