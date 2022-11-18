22b1cf70a03093dccd3bc842b64b29a521486c7a
Lee Lazarecky <leelazarecky@gmail.com>
2022-11-17 21:11:17 -0500

    Add more styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
 export default function Header() {
     return (
-        <div className={"flex justify-center items-center h-24 w-full bg-white text-lg font-bold"}>
-            Diablo II: MAX DPS CALCULATOR
+        <div
+            className={"h-32 w-full bg-white text-lg font-bold bg-stone-800 text-white px-12"}>
+
+            <div className={"flex items-start flex-col h-full justify-center "}>
+                <h1 className={"text-3xl"}>Diablo II</h1>
+                <span className={"opacity-60"}>Physical Damage Calculator</span>
+            </div>
+
         </div>
     )
 }
