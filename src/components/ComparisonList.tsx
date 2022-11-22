030cf45d8c43167f5d61fb426f19ec6fbd154cc8
Lee Lazarecky <leelazarecky@gmail.com>
2022-11-21 20:21:36 -0500

    Reorder layout of comparison list

--- src/components/ComparisonList.tsx
+++ src/components/ComparisonList.tsx
@@ -1,26 +1,31 @@
-import _ from "lodash";
 import WeaponItem from "./WeaponItem";
 import {FAKE_WEAPONS_TO_TEST} from "../data/data";
+import _ from "lodash";
 
 export default function ComparisonList() {
     const CompareHeader = () => (
         <div
             className={
-                "flex flex-row items-center justify-between border-y-4 border-[#4B4B4B] bg-[#2C2C2C] p-4"
+                "flex flex-row items-center justify-between border-y-4 border-[#4B4B4B] bg-[#2C2C2C] px-6 py-8"
             }
         >
             <button
                 className={
                     "flex h-full items-center justify-center border-4 border-black bg-white bg-[#272020] py-2 px-16 text-xl text-xl font-bold text-white"
                 }
+                onClick={() => {
+                    alert("NOT WORKING!");
+                }}
             >
                 Compare Weapons
             </button>
-            <div className={"flex flex-row gap-4"}>
-                {_.range(3).map((it) => (
-                    <div key={it} className={"h-10 w-28 bg-white"}/>
-                ))}
-            </div>
+
+            <label className={"relative w-36"}>
+        <span className={"absolute -top-6 text-sm font-bold text-white"}>
+          Critical Chance %
+        </span>
+                <input className={"h-10 w-full bg-white"} type={"number"}/>
+            </label>
         </div>
     );
 
@@ -32,8 +37,17 @@ export default function ComparisonList() {
         >
             <CompareHeader/>
 
-            <div className={"flex flex-col items-center gap-4 py-8 px-32"}>
-                <h1 className={"mb-4 text-3xl font-bold text-white"}>Results</h1>
+            <div className={"flex flex-col items-center gap-4 py-8 px-10"}>
+                <div className={"relative mb-4 w-full"}>
+                    <h1 className={"text-3xl font-bold text-white flex-1 text-center"}>Results</h1>
+                    <div
+                        className={"absolute right-0 top-0 bottom-0 flex flex-row items-center gap-2"}
+                    >
+                        {_.range(3).map((it) => (
+                            <div key={it} className={"h-8 w-16 bg-white"}/>
+                        ))}
+                    </div>
+                </div>
                 {FAKE_WEAPONS_TO_TEST.map((it, index) => (
                     <WeaponItem
                         key={index}
