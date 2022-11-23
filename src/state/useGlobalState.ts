623f2342598f58f9094ae41b1151c931d83325e4
Lee Lazarecky <leelazarecky@gmail.com>
2022-11-23 18:31:38 -0500

    Add criticalStrikeChance to global state

--- src/state/useGlobalState.ts
+++ src/state/useGlobalState.ts
@@ -1,17 +1,22 @@
 import create from "zustand";
 import { Weapon } from "../types/Weapon";
-import {FAKE_WEAPONS_TO_TEST} from "../data/data";
+import { FAKE_WEAPONS_TO_TEST } from "../data/data";
 
 interface UseGlobalState {
   weapons: Weapon[];
+  criticalStrikeChance: string | undefined;
 }
 
 interface Actions {
   addWeapon: (weapon: Weapon) => any;
+  setCriticalStrikeChance: (chance: string) => void;
 }
 
 export const useGlobalState = create<UseGlobalState & Actions>((set) => ({
   weapons: [...FAKE_WEAPONS_TO_TEST],
   addWeapon: (weapon: Weapon) =>
     set((state) => ({ weapons: [...state.weapons, weapon] })),
+  criticalStrikeChance: undefined,
+  setCriticalStrikeChance: (chance: string) =>
+    set((state) => ({ criticalStrikeChance: chance })),
 }));
