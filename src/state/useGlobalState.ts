387851833039a6590e2260c2d98d822b372a8624
Lee Lazarecky <leelazarecky@gmail.com>
2022-11-23 18:32:32 -0500

    Fix interface naming issue

--- src/state/useGlobalState.ts
+++ src/state/useGlobalState.ts
@@ -2,7 +2,7 @@ import create from "zustand";
 import { Weapon } from "../types/Weapon";
 import { FAKE_WEAPONS_TO_TEST } from "../data/data";
 
-interface UseGlobalState {
+interface State {
   weapons: Weapon[];
   criticalStrikeChance: string | undefined;
 }
@@ -12,7 +12,7 @@ interface Actions {
   setCriticalStrikeChance: (chance: string) => void;
 }
 
-export const useGlobalState = create<UseGlobalState & Actions>((set) => ({
+export const useGlobalState = create<State & Actions>((set) => ({
   weapons: [...FAKE_WEAPONS_TO_TEST],
   addWeapon: (weapon: Weapon) =>
     set((state) => ({ weapons: [...state.weapons, weapon] })),
